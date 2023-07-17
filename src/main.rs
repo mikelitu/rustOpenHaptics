37940b461edafa0e2ad6b93e827f4d9331368450
mikelitu <mikelitubiomed@gmail.com>
2023-07-17 17:18:51 +0100

    added new functionalities

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 use rust_open_haptics::HapticDevice;
+use rust_open_haptics::open_library;
+use std::{thread, time};
 
 fn main() {
     
+    let lib_name = "libHD.so";
+
     let device = HapticDevice{
-        lib_name: String::from("libHD.so"),
         position: Vec::with_capacity(3),
         joints: Vec::with_capacity(3),
         gimbals: Vec::with_capacity(3),
-        button: false
+        button: false,
+        lib: open_library(lib_name).unwrap()
     };
-    println!("The ID from the connected device is {:?}", device.init().unwrap());
+    let id = device.init().unwrap();
+    println!("The ID from the connected device is {:?}", id);
+    
+    device._begin_frame(id);
+
+    let mut i = 0;
+    while i < 1000 {
+        let button = device.button();
+        println!("{}", button);
+        i = i + 1;
+        thread::sleep(time::Duration::from_millis(1))
+    }
+
+    device._end_frame(id);
     device.close().unwrap();
 }
